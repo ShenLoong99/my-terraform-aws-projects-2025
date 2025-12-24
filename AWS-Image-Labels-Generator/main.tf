e22fdfe351863b62ba74c1f3822ae357833f552b
ShenLoong99 <yithuettsk29@gmail.com>
2025-12-24 20:40:45 +0800

    update architecture diagram & add lambda limit

--- AWS-Image-Labels-Generator/main.tf
+++ AWS-Image-Labels-Generator/main.tf
@@ -171,6 +171,8 @@ resource "aws_lambda_function" "rekognition_lambda" {
   function_name = "${var.project_name}-rekognition-lambda"
   role          = aws_iam_role.lambda_role.arn
   runtime       = "python3.11"
+  timeout       = 10  # Lambda Timeout Explicitly Set
+  memory_size   = 256 # Lambda Memory Explicitly Set
 
   filename         = data.archive_file.lambda_zip.output_path
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
